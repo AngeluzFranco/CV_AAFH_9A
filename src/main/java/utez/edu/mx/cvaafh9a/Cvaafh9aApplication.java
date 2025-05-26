5ea4b65a4dc6036776f58e415bdb2c8408aa2e32
AngeluzFranco <20223tn093@utez.edu.mx>
2025-05-26 17:44:16 -0600

    [FIRST COMMIT] Subida inicial del proyecto

---
+++ src/main/java/utez/edu/mx/cvaafh9a/Cvaafh9aApplication.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.cvaafh9a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cvaafh9aApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cvaafh9aApplication.class, args);
+    }
+
+}
---
+++ src/test/java/utez/edu/mx/cvaafh9a/Cvaafh9aApplicationTests.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.cvaafh9a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cvaafh9aApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
